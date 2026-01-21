c57ee8cfc5393e65f585f4122197a67eda425a04
artisticaipucbochicasur-code <artisticaipucbochicasur@gmail.com>
2026-01-21 18:24:54 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener('push', (event) => {
+  let data = {};
+  try { data = event.data ? event.data.json() : {}; } catch (e) {}
+
+  const title = data.title || "DECOM";
+  const options = {
+    body: data.body || "",
+    icon: data.icon || "/icon-192.png",
+    badge: data.badge || "/icon-192.png",
+    data: { url: data.url || "/" }
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  const url = (event.notification.data && event.notification.data.url) || "/";
+  event.waitUntil(clients.openWindow(url));
+});
