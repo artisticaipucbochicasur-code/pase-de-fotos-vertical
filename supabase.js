e440724fa155e07d4df0fd22e928f86b78d5a406
artisticaipucbochicasur-code <artisticaipucbochicasur@gmail.com>
2026-07-22 11:52:24 -0500

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -5,7 +5,7 @@
 
 const SUPABASE_URL = "https://kjbsaiaiuafbbleruitj.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_K9XAeQmzxK7-jANNEmrd0Q_u3WBFjxT";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtqYnNhaWFpdWFmYmJsZXJ1aXRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MjcxNjIsImV4cCI6MjEwMDMwMzE2Mn0.1L8JGyvSzw0aOTPm2jX921f-U_uw-P0a0jpkE4pEKCA";
 
 
 const supabaseClient = supabase.createClient(
