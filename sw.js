b55da753b2402663a5f281df81e7a6c4f437b6ce
artisticaipucbochicasur-code <artisticaipucbochicasur@gmail.com>
2026-07-31 15:34:30 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,27 @@
+const CACHE_VERSION = "decom-v2.4.2";
 self.addEventListener("install", (event) => {
   // ✅ Asegura que el SW nuevo tome control lo antes posible
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  // ✅ Asegura que controle clientes existentes (evita “reinicios” raros)
-  event.waitUntil(self.clients.claim());
+
+  event.waitUntil(
+    (async () => {
+
+      const keys = await caches.keys();
+
+      await Promise.all(
+        keys
+        .filter(key => key !== CACHE_VERSION)
+        .map(key => caches.delete(key))
+      );
+
+      await self.clients.claim();
+
+    })()
+  );
+
 });
 
 self.addEventListener("push", (event) => {
